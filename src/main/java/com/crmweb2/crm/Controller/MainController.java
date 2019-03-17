32411c1fe7a325fcfa28d5afa21e43a95b8d50f5
denys <denver0780@bigmir.net>
2019-03-17 18:41:28 +0200

    test

---
+++ src/main/java/com/crmweb2/crm/Controller/MainController.java
@@ -0,0 +1,12 @@
+package com.crmweb2.crm.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping
+    public String getTestPage(){
+        return "test";
+    }
+}
