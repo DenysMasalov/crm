07d627302f3da41016ca79b9a0c5ab0d922dd381
denys <denver0780@bigmir.net>
2019-03-17 19:34:07 +0200

    test

--- src/main/java/com/crmweb2/crm/Controller/MainController.java
+++ src/main/java/com/crmweb2/crm/Controller/MainController.java
@@ -1,7 +1,10 @@
 package com.crmweb2.crm.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -21,5 +24,21 @@ public class MainController {
     public String getSecondSite(){
 
         return "second";
+    }
+
+    @PostMapping("/zoho")
+    public String postZohoSite(@RequestParam String company,
+                               @RequestParam String lname,
+                               @RequestParam String city,
+                               @RequestParam String email,
+                               @RequestParam String type, Model model){
+
+        model.addAttribute("company", company);
+        model.addAttribute("lname", lname);
+        model.addAttribute("city", city);
+        model.addAttribute("email", email);
+        model.addAttribute("type", type);
+
+        return "zoho";
     }
 }
